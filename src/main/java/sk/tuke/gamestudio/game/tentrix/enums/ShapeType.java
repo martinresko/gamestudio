13b2c5cce5db32cef08e683a894a2c7fd53ddb94
martinresko <martinresko@users.noreply.github.com>
2021-03-23 17:32:50 +0100

    shape patter polished

--- src/main/java/sk/tuke/gamestudio/game/tentrix/enums/ShapeType.java
+++ src/main/java/sk/tuke/gamestudio/game/tentrix/enums/ShapeType.java
@@ -31,4 +31,12 @@ public enum ShapeType {
     public static final int[][] L_COORDINATES = {{0, 0, 1, 0, 0, 1, 0, 2}, {0, 0, 0, 1, 1, 1, 2, 1}, {1, 0, 1, 1, 0, 1, 1, 2}, {0, 0, 1, 0, 2, 0, 2, 1}};
     public static final int[][] T_COORDINATES = {{1, 0, 0, 1, 1, 1, 2, 1}, {1, 0, 0, 1, 1, 1, 1, 2}, {0, 0, 1, 0, 2, 0, 1, 1}, {0, 0, 0, 1, 1, 1, 0, 2}};
 
+    public static final String[] O_SHAPE = {"XX \nXX", "XX \nXX", "XX \nXX", "XX \nXX"};
+    public static final String[] I_SHAPE = {"X\nX\nX\nX\n", "XXXX", "X\nX\nX\nX\n", "XXXX"};
+    public static final String[] S_SHAPE = {" XX \nXX ", "X \nXX\n X", " XX \nXX ", "X \nXX\n X"};
+    public static final String[] Z_SHAPE = {"XX \n XX", " X\nXX\nX ", "XX \n XX", " X\nXX\nX "};
+    public static final String[] J_SHAPE = {" X\n X\nXX", "X  \nXXX", "XX\nX \nX ", "XXX\n  X"};
+    public static final String[] L_SHAPE = {"X \nX \nXX", "XXX\nX  ", "XX\n X\n X", "  X\nXXX"};
+    public static final String[] T_SHAPE = {"XXX\n X ", " X\nXX\n X", " X \nXXX", "X \nXX\nX "};
+
 }
--- src/main/java/sk/tuke/gamestudio/game/tentrix/models/Shape.java
+++ src/main/java/sk/tuke/gamestudio/game/tentrix/models/Shape.java
@@ -8,12 +8,19 @@ import sk.tuke.gamestudio.game.tentrix.enums.ShapeType;
 import java.util.Random;
 
 import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.I_COORDINATES;
+import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.I_SHAPE;
 import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.J_COORDINATES;
+import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.J_SHAPE;
 import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.L_COORDINATES;
+import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.L_SHAPE;
 import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.O_COORDINATES;
+import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.O_SHAPE;
 import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.S_COORDINATES;
+import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.S_SHAPE;
 import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.T_COORDINATES;
+import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.T_SHAPE;
 import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.Z_COORDINATES;
+import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.Z_SHAPE;
 import static sk.tuke.gamestudio.game.tentrix.enums.ShapeType.randomShape;
 
 @Getter
@@ -35,46 +42,25 @@ public class Shape {
 
         switch (shape.type) {
             case O:
-                shape.pattern[0] = "XX \nXX";
-                shape.pattern[1] = "XX \nXX";
-                shape.pattern[2] = "XX \nXX";
-                shape.pattern[3] = "XX \nXX";
+                shape.pattern = O_SHAPE.clone();
                 break;
             case I:
-                shape.pattern[0] = "X\nX\nX\nX\n";
-                shape.pattern[1] = "XXXX";
-                shape.pattern[2] = "X\nX\nX\nX\n";
-                shape.pattern[3] = "XXXX";
+                shape.pattern = I_SHAPE.clone();
                 break;
             case S:
-                shape.pattern[0] = " XX \nXX ";
-                shape.pattern[1] = "X \nXX\n X";
-                shape.pattern[2] = " XX \nXX ";
-                shape.pattern[3] = "X \nXX\n X";
+                shape.pattern = S_SHAPE.clone();
                 break;
             case Z:
-                shape.pattern[0] = "XX \n XX";
-                shape.pattern[1] = " X\nXX\nX ";
-                shape.pattern[2] = "XX \n XX";
-                shape.pattern[3] = " X\nXX\nX ";
+                shape.pattern = Z_SHAPE.clone();
                 break;
             case J:
-                shape.pattern[0] = " X\n X\nXX";
-                shape.pattern[1] = "X  \nXXX";
-                shape.pattern[2] = "XX\nX \nX ";
-                shape.pattern[3] = "XXX\n  X";
+                shape.pattern = J_SHAPE.clone();
                 break;
             case L:
-                shape.pattern[0] = "X \nX \nXX";
-                shape.pattern[1] = "XXX\nX  ";
-                shape.pattern[2] = "XX\n X\n X";
-                shape.pattern[3] = "  X\nXXX";
+                shape.pattern = L_SHAPE.clone();
                 break;
             case T:
-                shape.pattern[0] = "XXX\n X ";
-                shape.pattern[1] = " X\nXX\n X";
-                shape.pattern[2] = " X \nXXX";
-                shape.pattern[3] = "X \nXX\nX ";
+                shape.pattern = T_SHAPE.clone();
                 break;
         }
 
